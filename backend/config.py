399e84a9a87ad688e6cc58f54a5f68f5512db225
seggi sms <seggimarugira@gmail.com>
2021-04-04 08:56:38 -0400

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -6,10 +6,8 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     SQLAKCHEMY_COMMIT_ON_TEARDOWN = True 
     FLASK_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASK_MAIL_SENDER = 'Flasky Admin <nankim45@gmail.com>'
+    FLASK_MAIL_SENDER = 'Flasky Admin <any@gmail.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
-    # PROPAGATE_EXCEPTIONS = True # Prevent expiration exceptions
-    # JWT_SECRET_KEY = "difficult-to-guess-cafe-py"
     @staticmethod 
     def init_app(app):
         pass 
@@ -19,18 +17,7 @@ class DevelopmentConfig(Config):
     PROPAGATE_EXCEPTIONS = True # Prevent expiration exceptions
     JWT_SECRET_KEY = "difficult-to-guess-cafe-py"
     JWT_ACCESS_TOKEN_EXPIRES = False
-    UPLOAD_FOLDER = '~/nankimmain/PAH/pash/public/images/'
-    # JWT_ACCESS_TOKEN_EXPIRES = timedelta(7)
-    # MAIL_SERVER = 'smtp.google.com'
-    # MAIL_PORT = 587
-    # MAIL_USE_TLS = True 
-    # MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
-    #     'sqlite:///' + os.path.join(basedir, 'nk-ngoozo.sqlite')
-
-    # SQLALCHEMY_DATABASE_URI = "mysql://root:@127.0.0.1/nk-ngoozoability"
+#     UPLOAD_FOLDER = '~/nankimmain/PAH/pash/public/images/'
     SQLALCHEMY_DATABASE_URI = "mysql://root:@127.0.0.1/nk-hpas" 
 
     MYSQL_DATABASE_HOST = 'localhost'
@@ -43,18 +30,32 @@ class TestingConfig(Config):
     TESTING = True 
     PROPAGATE_EXCEPTIONS = True # Prevent expiration exceptions
     JWT_SECRET_KEY = "difficult-to-guess-cafe-py"
-    UPLOAD_FOLDER = '~/nankimmain/PAH/pash/public/images/'
+#     UPLOAD_FOLDER = '~/nankimmain/PAH/pash/public/images/'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'nk-hpas.sqlite')
+    
+    SQLALCHEMY_DATABASE_URI = "mysql://root:@127.0.0.1/nk-hpas" 
+
+    MYSQL_DATABASE_HOST = 'localhost'
+    MYSQL_DATABASE_USER = 'root'
+    MYSQL_DATABASE_PASSWORD = ''
+    MYSQL_DATABASE_DB = 'nk-hpas'
 
 
 class ProductionConfig(Config):
     PROPAGATE_EXCEPTIONS = True # Prevent expiration exceptions
     JWT_SECRET_KEY = "difficult-to-guess-cafe-py"
     UPLOAD_FOLDER = '~/nankimmain/PAH/pash/public/images/'
-    # JWT_ACCESS_TOKEN_EXPIRES = timedelta(7)
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(7)
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'snk-hpas.sqlite')
+    
+    SQLALCHEMY_DATABASE_URI = "mysql://root:@127.0.0.1/nk-hpas" 
+
+    MYSQL_DATABASE_HOST = 'localhost'
+    MYSQL_DATABASE_USER = 'root'
+    MYSQL_DATABASE_PASSWORD = ''
+    MYSQL_DATABASE_DB = 'nk-hpas'
 
 
 config = {
